b254335fc66705a57fc843abc9e938abdfe55e76
joel <joeltimcampbell@gmail.com>
2025-09-30 15:51:43 +0100

    begining stuff

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p className="paragraph">WELCOME TO REACT </p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello
         </p>
         <a
           className="App-link"
